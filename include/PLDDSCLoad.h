d75f58ef3eb036c3ba4990af26c79729365535a5
Siteswap <44817042+siteswapv4@users.noreply.github.com>
2025-09-26 12:18:58 +0200

    Delete include directory

--- include/PLDDSCLoad.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "PLD.h"
-#include "PLDNotes.h"
-#include "PLDUtils.h"
-#include "PLDPPDLoad.h"
-
-
-int PLD_LoadDSCChart(PLD_Song* songInfo);
--- include/PLDMenu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "PLD.h"
-#include "PLDUtils.h"
-
-#include <PLD_context.h>
-
-
-PLD_MenuInput PLD_GetMenuPressedInput(PLD_Context* context, SDL_Event* event);
